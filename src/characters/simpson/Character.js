9810e54d6564d4857a152356cbdc2baff6ba2986
itZahar <85490326+itZahar@users.noreply.github.com>
2022-06-23 17:18:36 +0300

    Delete src/characters directory
    delate all from master

--- src/characters/simpson/Character.js
+++
@@ -1,9 +0,0 @@
-function Character(props) {
-    let {name,pic} = props;
-    return (
-    <div>
-        <h2> {name}</h2>
-        <img src={pic} alt={'simpsons family'}/>
-    </div>)
-    }
-export default Character
