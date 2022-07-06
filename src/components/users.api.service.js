c6c8e7b8223506c155d654820ce71a617380cc31
itZahar <zahargats1998@gmail.com>
2022-07-06 14:05:55 +0300

    hw_4_1

---
+++ src/components/users.api.service.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+let axiosInstance = axios.create({
+    baseURL:"http://jsonplaceholder.typicode.com/users",
+    headers:{"Content-Type":"application/json; charset=UTF-8"}
+})
+const saveUser = (data) => axiosInstance.post('',{...data})
+
+export {saveUser}
+
+
+
